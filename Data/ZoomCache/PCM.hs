a5a7f998c6c903b276b98df890a8a4d3a13c21b6
Conrad Parker <conrad@metadecks.org>
2011-11-04 16:16:37 +0800

    PCM: add pcmMappings

--- Data/ZoomCache/PCM.hs
+++ Data/ZoomCache/PCM.hs
@@ -3,9 +3,21 @@
 module Data.ZoomCache.PCM (
     -- * PCM Type
       PCM(..)
+
+    -- * Track mappings
+    , pcmMappings
 ) where
 
 import Data.ZoomCache.PCM.Types
 
 import Data.ZoomCache.PCM.Double()
 import Data.ZoomCache.PCM.Int()
+
+import Data.ByteString.Lazy (ByteString)
+import Data.ZoomCache (TrackType, ttMapping)
+
+pcmMappings :: [ByteString -> Maybe TrackType]
+pcmMappings =
+    [ ttMapping (undefined :: PCM Double)
+    , ttMapping (undefined :: PCM Int)
+    ]
